26e6d5f5ae461a45f420d18051de72d5c40611c8
kevin lang <langkevina@gmail.com>
2019-07-21 18:56:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,18 +32,18 @@ class Dog
       self.update
     end
   end
-  # def self.create(name:, breed:)
-  #   song = Song.new(name, breed)
-  #   song.save
-  #   song
-  # end
+  def self.create(params)
+    dog = Dog.new(name: params["name"], breed: params["breed"] )
+    dog.save
+    dog
+  end
 
   def self.find_by_id(id)
     sql = "SELECT * FROM dogs WHERE id = ?"
     result = DB[:conn].execute(sql, id)[0]
     Dog.new(result[0], result[1], result[2])
   end
-
+ 
   def update
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.breed, self.id)
