5164a3a9863eb55084f6c0352c047afc07be6cd5
kevin lang <langkevina@gmail.com>
2019-07-21 18:36:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,4 +4,15 @@ class Dog
   def initialize(id: = nil,name:, breed:)
 
   end
+  def self.create_table
+      sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
+      SQL
+      DB[:conn].execute(sql)
+    end
+
 end
