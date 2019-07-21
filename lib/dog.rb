3549921859a8b8d4092bf74c7e239c5707c23529
kevin lang <langkevina@gmail.com>
2019-07-21 19:04:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,9 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
   def initialize(id = nil,name:, breed:)
-
+    @id = id
+    @name = name
+    @breed = breed
   end
   def self.create_table
     sql = <<-SQL
