416bd857b40b6827e8e6cb341d5682e3bbf4af3f
kevin lang <langkevina@gmail.com>
2019-07-21 19:05:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,7 +29,7 @@ class Dog
         VALUES (?, ?)
       SQL
       DB[:conn].execute(sql, self.name, self.breed)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     else
       self.update
     end
@@ -61,7 +61,7 @@ class Dog
     dog
   end
   def self.find_or_create_by(name:, breed:)
-    dog = DB[:conn].execute("SELECT * FROM songs WHERE name = ? AND breed = ?", name, breed)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
       dog_data = dog[0]
       dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
